5f5e407827bf600fe43128bfdeb4b672f293858b
theo m <theo@mantz.io>
2021-10-18 21:24:34 -0400

    Add comments

---
+++ comments/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // Heres a simple comment:
+    // Hello, world!
+
+    // In Rust, the idiomatic comment style starts a comment with two slashes, and 
+    // the comment continues until the end of the line. For comments that extend 
+    // beyond a single line you need to include `//` on each line
+
+    // comments can be placed at the end of lines containing code:__rust_force_expr!
+    
+    let lucky_number = 7; // I'm feeling lucky today
+
+    // You'll most often see comments on a separate line above the code its annotating
+
+    // Rust has another type of comment, documentation comments, which are covered
+    // in "Publishing a Crate to Crates.io" section of chapter 14
+}
